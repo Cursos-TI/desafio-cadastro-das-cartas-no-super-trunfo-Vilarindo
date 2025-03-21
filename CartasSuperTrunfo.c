4dc6d791723144526fa5b57e621a78880e6b490f
Vilarindo <74842145+Vilarindo@users.noreply.github.com>
2025-03-21 18:31:49 +0000

    Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,12 @@
 
 int main() {
 
-    printf("Super Trunfo - Novato\n");
+    printf("Super Trunfo - Aventureiro\n");
 
     // Definindo as variáveis
     int cod_cidade1, populacao1, pontos_turisticos1, cod_cidade2, populacao2, pontos_turisticos2;
     char nome1[50], nome2[50];
-    float area1, pib1, area2, pib2;
+    float area1, pib1, area2, pib2, densPop1, densPop2, pibCap1, pibCap2;
     
     // Cadastro das Cartas:
     // Solicitando ao usuário as informações de cada carta
@@ -34,6 +34,9 @@ int main() {
     printf("Digite o pib da cidade: \n");
     scanf("%f", &pib1);
 
+    densPop1 = populacao1/area1;
+    pibCap1 = pib1/populacao1;
+
     //que falta faz um loop, não é?
 
     printf("\n");
@@ -56,14 +59,17 @@ int main() {
     printf("Digite o pib da cidade: \n");
     scanf("%f", &pib2);
 
+    densPop2 = populacao2/area2;
+    pibCap2 = pib2/populacao2;
+
     // Exibição das cartas
     printf("\n");
     printf("Informações da carta 1\n");
-    printf("Cidade: %s \nCódigo da Cidade: %d \nPopulação: %d \nPontos Turísitcos: %d \nÁrea: %.2f \nPIB: %.2f \n", nome1, cod_cidade1, populacao1, pontos_turisticos1, area1, pib1);
+    printf("Cidade: %s \nCódigo da Cidade: %d \nPopulação: %d \nPontos Turísitcos: %d \nÁrea: %.2f \nPIB: %.2f \nDensidade Populacional: %.2f \nPIB per capita: %.2f \n", nome1, cod_cidade1, populacao1, pontos_turisticos1, area1, pib1, densPop1, pibCap1);
     
     printf("\n");
     printf("Informações da carta 2\n");
-    printf("Cidade: %s \nCódigo da Cidade: %d \nPopulação: %d \nPontos Turísitcos: %d \nÁrea: %.2f \nPIB: %.2f \n", nome2, cod_cidade2, populacao2, pontos_turisticos2, area2, pib2);
+    printf("Cidade: %s \nCódigo da Cidade: %d \nPopulação: %d \nPontos Turísitcos: %d \nÁrea: %.2f \nPIB: %.2f \nDensidade Populacional: %.2f \nPIB per capita: %.2f \n", nome2, cod_cidade2, populacao2, pontos_turisticos2, area2, pib2, densPop2, pibCap2);
 
     return 0;
 }
